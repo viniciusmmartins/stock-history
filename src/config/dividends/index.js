98a3655adb09de7a43a54fbddb3c727065d53c6c
Otavio Henrique Pires Costa <othecos@ibm.com>
2020-05-12 08:07:53 -0300

    Dividend responde refactor

--- src/config/dividends/index.js
+++ src/config/dividends/index.js
@@ -1,9 +1,60 @@
-export const DIVIDENDS_CALENDAR_URL = 'http://www.dividendobr.com/tabex.php'
+export const DIVIDENDS_CALENDAR_URL = 'http://dividendobr.com/tabexregra2.php'
 
 export const DIVIDENDS_DICTIONARY = [
-    { label: 'Data da', key: 'date_aproval' },
-    { label: 'Tipo', key: 'payment_type' },
-    { label: 'Data Ex', key: 'date_ex' },
-    { label: 'Previsão de', key: 'date_payment' },
+    { label: 'símbolo', key: 'code',type: 'string' },
+    { label: 'Data da', key: 'date_aproval',type: 'date' },
+    { label: 'Tipo', key: 'payment_type',type: 'string' },
+    { label: 'Data Ex', key: 'date_ex',type: 'date' },
+    { label: 'Previsão de', key: 'date_payment',type: 'date' },
     { label: 'Valor', key: 'value' },
+]
+export const months = [
+    {
+        label: 'Janeiro',
+        format: '00'
+    },
+    {
+        label: 'Fevereiro',
+        format: '01'
+    },
+    {
+        label: 'Março',
+        format: '02'
+    },
+    {
+        label: 'Abril',
+        format: '03'
+    },
+    {
+        label: 'Maio',
+        format: '04'
+    },
+    {
+        label: 'Junho',
+        format: '05'
+    },
+    {
+        label: 'Julho',
+        format: '06'
+    },
+    {
+        label: 'Agosto',
+        format: '07'
+    },
+    {
+        label: 'Setembro',
+        format: '08'
+    },
+    {
+        label: 'Outubro',
+        format: '09'
+    },
+    {
+        label: 'Novembro',
+        format: '10'
+    },
+    {
+        label: 'Dezembro',
+        format: '11'
+    }
 ]
--- src/controller/scrapper/dividends/index.js
+++ src/controller/scrapper/dividends/index.js
@@ -1,7 +1,8 @@
 import ScrapperController from "..";
 import * as cheerio from 'cheerio';
 import * as fetch from 'node-fetch'
-import { DIVIDENDS_CALENDAR_URL,DIVIDENDS_DICTIONARY } from "../../../config/dividends";
+import { DIVIDENDS_CALENDAR_URL,DIVIDENDS_DICTIONARY,months} from "../../../config/dividends";
+import moment from "moment";
 export default class DividendsController extends ScrapperController {
     constructor() {
         super()
@@ -12,12 +13,12 @@ export default class DividendsController extends ScrapperController {
             if (response) {
                 const html = await response.text()
                 const $ = cheerio.load(html);
-                const tables = $('#dilist tbody')
+                const tables = $('table tbody')
                 if (tables) {
                     const result = []
                     for (let index = 0; index < tables.length; index++) {
                         let table = tables[index];
-                        let dividend = []
+                        let dividend = {}
                         const rows = table.children
                         rows.forEach((row,index) => {
                             try {
@@ -25,22 +26,34 @@ export default class DividendsController extends ScrapperController {
                                 const label = $(column[0]).text()
                                 const value =  $(column[1]).text()
                                 const word = DIVIDENDS_DICTIONARY.find((div_dic) => label.includes(div_dic.label))
-                                console.log(word)
                                 if(!word) return;
                                 let field = {
-                                    key: word ? word.key : label,
-                                    value
+                                    [word ? word.key : label]: value
                                 }
                                 if(word.key == 'value'){
-                                    const nextValue = $(rows[index+1].children.filter(column => column.name == 'td')[0]).text()
+                                    let nextValue = ''
+                                    try{
+                                        nextValue = $(rows[index+1].children.filter(column => column.name == 'td')[0]).text()
+                                    }catch{}
+                                    field = this.formatStockValues(word,value,nextValue)
+                                }
+                                else if(word.key == 'code'){
+                                    const formattedValue = value.match(/.*?(?= *-)/)[0]
                                     field = {
-                                        ...field,
-                                        value: [value, nextValue]
+                                        [word ? word.key : label]: formattedValue,
                                     }
                                 }
+                                if(word.type == 'date'){
+                                    field = this.formatDate(word,value)
+                                }
+                                dividend = {
+                                    ...dividend,
+                                    ...field
+                                }
+                            } catch(err){
+                                console.error(err);
                                 
-                                dividend.push(field)
-                            } catch{ }
+                             }
                         })
                         result.push(dividend)
                     }
@@ -55,5 +68,43 @@ export default class DividendsController extends ScrapperController {
         catch (err) {
             throw { code: err.code, message: err.message }
         }
+    }
+    formatDate(word,value){
+        let field = {}
+        const regexp = new RegExp(`([0-3][0-9]).*(${months.map(month => month.label).join('|')}).*([0-9]{4})`)
+        const dates = value.match(regexp)
+        if(dates && dates.length > 0){
+            const day = dates[1]
+            const monthNumber = months.find((month)=> month.label == dates[2])
+            let month = 0;
+            if(monthNumber){
+                month = months.find((month)=> month.label == dates[2]).format
+            }
+            const year = dates[3]
+            field = {
+                [word ? word.key : label]: new Date(year,month,day)
+            }
+        }
+        return field
+    }
+    formatStockValues(word,firtColumn,secondColumn){
+        let field = {}
+        let [firstColumnValue,firstColumnStockType ] = firtColumn.match(/([0-9](,|),[0-9]+)|(PN|ON|UNIT)/gi)
+        if(secondColumn){
+            let [secondColumnValue,secondColumnStockType] = secondColumn.match(/([0-9],[0-9]+)|(PN|ON|UNIT)/gi)
+            field = {
+                [word ? word.key : label]: {
+                    [firstColumnStockType ? firstColumnStockType : 'ON']: parseFloat(firstColumnValue.replace(',','.')),
+                    [secondColumnStockType]:  parseFloat(secondColumnValue.replace(',','.'))
+                }
+            }
+        }else{
+            field = {
+                [word ? word.key : label]:{ 
+                    [firstColumnStockType ? firstColumnStockType : 'ON']:  parseFloat(firstColumnValue.replace(',','.')),
+                }
+            }
+        }
+        return field
     }
 }
